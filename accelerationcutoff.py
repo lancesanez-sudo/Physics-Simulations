f479d2ad06cab5f7c7b0aa3808858e86dd944be6
Lance Sanez <lancesanez@gmail.com>
2026-07-24 22:53:53 -0400

    Add files via upload

---
+++ accelerationcutoff.py
@@ -0,0 +1,37 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+v = 0
+x = 0
+a = 2
+dt = 0.01
+t = np.arange(0, 10, dt)
+
+velocity = []
+displacement = []
+
+for time in t:
+  if time >= 5:
+    a = 0
+  else:
+    a = 2
+  v = v + a * dt
+  x = x + v * dt
+  velocity.append(v)
+  displacement.append(x)
+
+print("Velocities: ", velocity)
+print("Displacements: ", displacement)
+
+plt.plot(t, velocity)
+plt.xlabel("Time (s)")
+plt.ylabel("Velocity m/s")
+plt.title("Acceleration Cutoff Simulator (Velocity)")
+plt.show()
+
+plt.plot(t, displacement)
+plt.xlabel("Time (s)")
+plt.ylabel("Displacement (m)")
+plt.title("Acceleration Cutoff Simulator (Displacement)")
+plt.show()
